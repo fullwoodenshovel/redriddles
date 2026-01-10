aa1ebfa45e1b4be6d69f5015c2fb3ced3c5c97ac
fullwoodenshovel <otalqasem@gmail.com>
2026-01-10 23:49:54 +0100

    Rid old code

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,6 @@ use macroquad::prelude::*;
 use redriddles::ui::Frame;
 use redriddles::ui::main::Main;
 
-
-#[derive(PartialEq, Debug, Clone, Copy)]
-enum Focus {
-    Topbar,
-    Sidebar,
-    Picker,
-    Draw,
-}
-
-#[derive(PartialEq, Debug, Clone, Copy)]
-enum LastTouchFocus {
-    None,
-    SaveCol,
-    TypeCol,
-    Picker,
-}
-
 #[macroquad::main("Pixel Editor")]
 async fn main() {
     let mut frame = Frame::new::<Main>();
